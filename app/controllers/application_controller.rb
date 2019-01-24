44e2e69222b2903b356c83799b849a5b39833f3c
高橋　是高 <shcrad.kore@gmail.com>
2019-01-24 08:34:02 +0000

    application_controllerに非ログイン時リダイレクトさせるメソッド記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  
+  private
+  
+  def redirect_if_not_logged_in
+    return if logged_in?
+    flash[:notice] = "権限がありません"
+    redirect_to(feeds_url) # feeds_pathじゃダメなんだっけ？？？
+  end
 end
