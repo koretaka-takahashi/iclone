6c6bed5ab17fa7a8cee0d42de5ff045df1ca964d
高橋　是高 <shcrad.kore@gmail.com>
2019-01-28 07:55:47 +0000

    feedsのビューにindex.html.erb作成

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,6 +1,6 @@
 class Feed < ApplicationRecord
   belongs_to :user
-  mount_uploader :image, ImageUploader
+  # mount_uploader :image, ImageUploader
   validates :text, presence: true, length: {maximum: 255}
   validates :image, presence: true
 end
