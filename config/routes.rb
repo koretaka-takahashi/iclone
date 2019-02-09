981f409e508a413e52afffdb35ce1ae10992a387
高橋　是高 <shcrad.kore@gmail.com>
2019-02-09 04:46:27 +0000

    gitに画像を上げないように記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'favorites/create'
+
+  get 'favorites/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'feeds#index'
   resources :users
