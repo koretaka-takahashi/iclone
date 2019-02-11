364d1d0e9383160bde13be8a7172b838b3de501c
高橋　是高 <shcrad.kore@gmail.com>
2019-02-11 09:00:13 +0000

    お気に入り一覧ページ設置。エラーページ実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,8 @@
 class FavoritesController < ApplicationController
+  def index
+    @favorites = current_user.favorites.all.order(:created_at)
+  end
+  
   def create
     favorite = current_user.favorites.create(feed_id: params[:feed_id])
     redirect_to feed_path(favorite.feed_id), notice: "<<<#{favorite.feed.user.name}さんの投稿をお気に入り登録しました>>>"
